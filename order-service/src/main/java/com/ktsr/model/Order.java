4dc72913330332b76c54cf4f14dbda4564bb23a5
kasatejaswarreddy0512 <ktrreddy64@gmail.com>
2025-07-04 00:20:28 +0530

    order entity commit

---
+++ order-service/src/main/java/com/ktsr/model/Order.java
@@ -0,0 +1,27 @@
+package com.ktsr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.engine.internal.Cascade;
+
+import java.util.List;
+
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItems;
+}
